dce0a54eda349a94e2ed2af12703e6162813310d
gupta <tripti.gupta@interseroh.com>
2018-07-09 15:23:36 +0200

    create expenseapi

---
+++ expense/expenseapi/src/main/java/com/tpt/budgetbucket/expense/dtos/Item.java
@@ -0,0 +1,13 @@
+package com.tpt.budgetbucket.customer.server.domain;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Address {
+	private String itemId;
+	private String itemName;
+
+
+
+}
